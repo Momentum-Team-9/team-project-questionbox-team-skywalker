24dc978c8efba96106558d3b7e125e000eb3d679
sarah-wines <sarah.lodico.wines@gmail.com>
2021-09-15 22:51:38 -0400

    route to login and question list, take log-in credentials

--- questionbox/src/App.js
+++ questionbox/src/App.js
@@ -1,20 +1,25 @@
+import { useState, useEffect } from 'react';
 import { QuestionList } from './components/QuestionList.js';
 import { Login } from './components/Login.js';
 import { Header } from './components/Header.js';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { Profile } from './components/Profile.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  const [auth, setAuth] = useState('');
+
+  useEffect(() => {
+    console.log(auth);
+  }, [auth]);
   return (
     <Router>
       <div className="App">
         <Header />
-        <Route path="/questions" component={QuestionList} />
-        <Route path="/login" component={Login} />
+        <Switch>
+          <Route path="/questions" component={QuestionList} />
+          <Route path="/login" component={() => <Login setAuth={setAuth} />} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
       </div>
     </Router>
   );
--- questionbox/src/components/Header.js
+++ questionbox/src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 export function Header() {
   return (
     <nav>
       <h3>GreenThumb</h3>
       <ul className="nav-links">
-        <li>QuestionList</li>
-        <li>Login</li>
+        <Link to="/questions">
+          <li>QuestionList</li>
+        </Link>
+        <Link to="/login">
+          <li>Login</li>
+        </Link>
+        {/* <Link to="/profile">
+          <li>Profile</li>
+        </Link> */}
       </ul>
     </nav>
   );
