b34c8715513ff7f9ed48423dc02afa76229b7b82
mjbiener <michaelbiener@yahoo.com>
2021-09-16 09:17:45 -0400

    stuck making request XML Error

---
+++ questionbox/src/components/Question.js
@@ -0,0 +1,21 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { QuestionList } from './QuestionList'
+
+
+// export const Question = ({questions}) => {
+//     const [selectedQuestion, setSelectedQuestion] = useState (null)
+
+//     return (
+//         <div className="questions">
+//             <h2>Questions</h2>
+//             <div>
+//                 {questions.map((question) => (
+//                     <button onClick={() => (setSelectedQuestion (question.pk))} 
+//                     key={question.pk}>{question.title} 
+//                     </button>
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// }
--- questionbox/src/components/QuestionList.js
+++ questionbox/src/components/QuestionList.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { questions as questionsList } from '../data.js';
+// import { Question } from './Question'
 
 export const QuestionList = () => {
-  const [questions, setQuestions] = useState([questionsList]);
-  console.log(questions);
-  return null;
+  const [questions, setQuestions] = useState([]);
+  const [selectedQuestion, setSelectedQuestion] = useState (null)
+  
+  useEffect(() => {
+    axios.get(`https://questionbox-team-skywalker.herokuapp.com/api/questions`).then((response) => {
+        setQuestions(response.data)
+    });
+}, [questions]);
+
+  return (
+    <>
+    <div className="questions">
+        <h2>questions</h2>
+            {questions.map((question) => (
+              <div className="questionCard">
+                key={question.pk}
+                <p>{question.title}</p>
+          </div>
+            ))}
+    </div>
+    </>
+);
 };
