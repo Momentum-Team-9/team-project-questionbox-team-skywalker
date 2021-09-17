5c228965c80eb92842129587c39e52b3c5ab8994
sarah-wines <sarah.lodico.wines@gmail.com>
2021-09-17 14:31:25 -0400

    renders username

--- questionbox/src/components/Header.js
+++ questionbox/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 export function Header({ token, setAuth, clearStorage }) {
   return (
     <nav>
--- questionbox/src/components/Question.js
+++ questionbox/src/components/Question.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 // import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
 // import { QuestionList } from './QuestionList'
-
 
 // export const Question = ({questions}) => {
 //     const [selectedQuestion, setSelectedQuestion] = useState (null)
@@ -12,27 +10,11 @@
 //             <h2>Questions</h2>
 //             <div>
 //                 {questions.map((question) => (
-//                     <button onClick={() => (setSelectedQuestion (question.pk))} 
-//                     key={question.pk}>{question.title} 
+//                     <button onClick={() => (setSelectedQuestion (question.pk))}
+//                     key={question.pk}>{question.title}
 //                     </button>
 //                 ))}
 //             </div>
 //         </div>
 //     );
 // }
-=======
-import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-import { questions as questionList } from '../data.js';
-
-
-
-
-
-
-return (
-    <button onClick={() => (setSelectedQuestion (question.id))} 
-    key={question.id}>{question.title} 
-    </button>
-    )
->>>>>>> ddbf2e5008aa6bc972436ef70347a9ff5fc5410d
